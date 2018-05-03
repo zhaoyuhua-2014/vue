9f4b5192751f2bc7cb7a036411644f649cd710c1
zhaoyuhua <zhaoyuhua_2014@163.com>
2018-05-03 18:15:21 +0800

    1.新的项目内容

--- README.md
+++ README.md
@@ -1,1 +1,146 @@
-# vue
+# my-project
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+
+//  "dev": "webpack-dev-server --inline --progress --config build/webpack.dev.conf.js",
+//  "start": "npm run dev",
+//  "unit": "jest --config test/unit/jest.conf.js --coverage",
+//  "test": "npm run unit",
+//  "build": "node build/build.js"
+
+
+//  "autoprefixer": "^7.1.2",
+//  "babel-core": "^6.22.1",
+//  "babel-helper-vue-jsx-merge-props": "^2.0.3",
+//  "babel-jest": "^21.0.2",
+//  "babel-loader": "^7.1.1",
+//  "babel-plugin-dynamic-import-node": "^1.2.0",
+//  "babel-plugin-syntax-jsx": "^6.18.0",
+//  "babel-plugin-transform-es2015-modules-commonjs": "^6.26.0",
+//  "babel-plugin-transform-runtime": "^6.22.0",
+//  "babel-plugin-transform-vue-jsx": "^3.5.0",
+//  "babel-preset-env": "^1.3.2",
+//  "babel-preset-stage-2": "^6.22.0",
+//  "chalk": "^2.0.1",
+//  "copy-webpack-plugin": "^4.0.1",
+//  "css-loader": "^0.28.0",
+//  "extract-text-webpack-plugin": "^3.0.0",
+//  "file-loader": "^1.1.4",
+//  "friendly-errors-webpack-plugin": "^1.6.1",
+//  "html-webpack-plugin": "^2.30.1",
+//  "jest": "^22.0.4",
+//  "jest-serializer-vue": "^0.3.0",
+//  "node-notifier": "^5.1.2",
+//  "optimize-css-assets-webpack-plugin": "^3.2.0",
+//  "ora": "^1.2.0",
+//  "portfinder": "^1.0.13",
+//  "postcss-import": "^11.0.0",
+//  "postcss-loader": "^2.0.8",
+//  "postcss-url": "^7.2.1",
+//  "rimraf": "^2.6.0",
+//  "semver": "^5.3.0",
+//  "shelljs": "^0.7.6",
+//  "uglifyjs-webpack-plugin": "^1.1.1",
+//  "url-loader": "^0.5.8",
+//  "vue-jest": "^1.0.2",
+//  "vue-loader": "^13.3.0",
+//  "vue-style-loader": "^3.0.1",
+//  "vue-template-compiler": "^2.5.2",
+//  "webpack": "^3.6.0",
+//  "webpack-bundle-analyzer": "^2.9.0",
+//  "webpack-dev-server": "^2.9.1",
+//  "webpack-merge": "^4.1.0"
+
+//  "autoprefixer": "^7.1.5",
+//  "babel-core": "^6.26.0",
+//  "sw-precache-webpack-plugin": "^0.11.4",
+//  "babel-eslint": "^8.0.1",
+//  "babel-loader": "^7.1.2",
+//  "babel-plugin-transform-runtime": "^6.23.0",
+//  "babel-preset-env": "^1.6.0",
+//  "babel-preset-stage-2": "^6.24.1",
+//  "babel-register": "^6.26.0",
+//  "chalk": "^2.1.0",
+//  "connect-history-api-fallback": "^1.4.0",
+//  "copy-webpack-plugin": "^4.1.1",
+//  "css-loader": "^0.28.7",
+//  "cssnano": "^3.10.0",
+//  "eslint": "^4.9.0",
+//  "eslint-friendly-formatter": "^3.0.0",
+//  "eslint-loader": "^1.9.0",
+//  "eslint-plugin-html": "^3.2.2",
+//  "eslint-plugin-import": "^2.7.0",
+//  "eslint-plugin-node": "^5.2.0",
+//  "eslint-config-standard": "^10.2.1",
+//  "eslint-plugin-promise": "^3.6.0",
+//  "eslint-plugin-standard": "^3.0.1",
+//  "eventsource-polyfill": "^0.9.6",
+//  "express": "^4.16.2",
+//  "extract-text-webpack-plugin": "^3.0.0",
+//  "file-loader": "^1.1.5",
+//  "friendly-errors-webpack-plugin": "^1.6.1",
+//  "html-webpack-plugin": "^2.30.1",
+//  "http-proxy-middleware": "^0.17.4",
+//  "webpack-bundle-analyzer": "^2.9.0",
+//  "cross-env": "^5.0.5",
+//  "karma": "^1.7.1",
+//  "karma-coverage": "^1.1.1",
+//  "karma-mocha": "^1.3.0",
+//  "karma-phantomjs-launcher": "^1.0.4",
+//  "karma-phantomjs-shim": "^1.5.0",
+//  "karma-sinon-chai": "^1.3.2",
+//  "karma-sourcemap-loader": "^0.3.7",
+//  "karma-spec-reporter": "0.0.31",
+//  "karma-webpack": "^2.0.5",
+//  "mocha": "^4.0.1",
+//  "chai": "^4.1.2",
+//  "sinon": "^4.0.1",
+//  "sinon-chai": "^2.14.0",
+//  "inject-loader": "^3.0.1",
+//  "babel-plugin-istanbul": "^4.1.5",
+//  "phantomjs-prebuilt": "^2.1.15",
+//  "chromedriver": "^2.33.1",
+//  "cross-spawn": "^5.1.0",
+//  "nightwatch": "^0.9.16",
+//  "selenium-server": "^3.6.0",
+//  "semver": "^5.4.1",
+//  "shelljs": "^0.7.8",
+//  "opn": "^5.1.0",
+//  "optimize-css-assets-webpack-plugin": "^3.2.0",
+//  "ora": "^1.3.0",
+//  "rimraf": "^2.6.2",
+//  "url-loader": "^0.6.2",
+//  "vue-loader": "^13.3.0",
+//  "vue-style-loader": "^3.0.3",
+//  "vue-template-compiler": "^2.5.2",
+//  "webpack": "^3.7.1",
+//  "webpack-dev-middleware": "^1.12.0",
+//  "webpack-hot-middleware": "^2.19.1",
+//  "webpack-merge": "^4.1.0",
+//  "uglify-es": "^3.1.3"
---
+++ src/pages/index.vue
@@ -0,0 +1,278 @@
+<template>
+	<div class="index-wrap">
+		<div class="index-left">
+			<div class="index-left-block">
+				<h2>全部产品</h2>
+				<template v-for="product in productList">
+					<h3>{{product.title}}</h3>
+					<ul>
+						<li v-for="item in product.list">
+							<a :href="item.url">{{item.name}}</a>
+							<span v-if="item.hot" class="hot-tag">HOT</span>
+						</li>
+					</ul>
+					<div v-if="!product.last" class="hr"></div>
+				</template>
+			</div>
+			<div class="index-left-block lastest-news">
+				<h2>最新消息</h2>
+				<ul>
+					<li v-for="item in newList">
+						<a :href="item.title">{{item.name}}</a>
+						<span v-if="item.hot" class="hot-tag">HOT</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="index-right">
+			<div class="banner">banner</div>
+			<div class="index-board-list">
+				<dl class="index-board-item" v-for="item in boardList">
+					<dt><!--<img :src="item.src"/>--></dt>
+					<dd class="index-board-item-inner">
+						<h2>{{item.title}}</h2>
+						<p>{{item.desciption}}</p>
+						<div class="index-board-button"><a href="#/detail/analysis" class="button">立即购买</a></div>
+					</dd>
+				</dl>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		created : function(){
+			this.$http.get('/api/boardList')
+			.then(function(data){
+				console.log(data)
+			},function(err){
+				console.log(err)
+			})
+		},
+		data () {
+			return {
+				productList : {
+					pc:{
+						'title':'PC产品',
+						'last':false,
+						'list' : [
+							{
+								'name':'数据统计',
+								'url':'#'
+							},
+							{
+								'name':'数据预测',
+								'url':'#'
+							},
+							{
+								'name':'数据分析',
+								'url':'#'
+							},
+							{
+								'name':'广告发布',
+								'url':'#'
+							}
+						]
+					},
+					app:{
+						'title':'APP产品',
+						'last':true,
+						'list' : [
+							{
+								'name':'数据统计',
+								'url':'#',
+								'hot':true
+							},
+							{
+								'name':'数据预测',
+								'url':'#'
+							},
+							{
+								'name':'数据分析',
+								'url':'#'
+							},
+							{
+								'name':'广告发布',
+								'url':'#'
+							}
+						]
+					}
+				},
+				newList : [
+					{
+						'name':'数据统计',
+						'url':'#',
+						'hot':true
+					},
+					{
+						'name':'数据预测',
+						'url':'#'
+					},
+					{
+						'name':'数据分析',
+						'url':'#'
+					},
+					{
+						'name':'广告发布',
+						'url':'#'
+					}
+				],
+				boardList:[
+					{
+						title:'产品一',
+						desciption:'产品一',
+						src:'../assets/images/1.png',
+						saleout:false
+					},
+					{
+						title:'产品二',
+						desciption:'产品二',
+						src:'../assets/images/2.png',
+						saleout:false
+					},
+					{
+						title:'产品三',
+						desciption:'产品三',
+						src:'../assets/images/3.png',
+						saleout:false
+					},
+					{
+						title:'产品四',
+						desciption:'产品四',
+						src:'assets/images/4.png',
+						saleout:false
+					},
+				]
+			}
+		}
+	}
+</script>
+
+<style>
+	.index-wrap {
+	  width: 1000px;
+	  margin: 0 auto;
+	  overflow: hidden;
+	}
+	.index-left {
+	  float: left;
+	  width: 30%;
+	  text-align: left;
+	}
+	.index-right {
+	  float: left;
+	  width: 70%;
+	}
+	.index-left-block {
+	  margin: 0 15px 15px 15px;
+	  background: #fff;
+	  box-shadow: 0 0 1px #ddd;
+	  border-radius: 10px;
+	}
+	.index-left-block .hr {
+	  margin-bottom: 20px;
+	}
+	.index-left-block h2 {
+	  background: #4fc08d;
+	  color: #fff;
+	  padding: 10px 15px;
+	  margin-bottom: 20px;
+	}
+	.index-left-block h3 {
+	  padding: 0 15px 5px 15px;
+	  font-weight: bold;
+	  color: #222;
+	}
+	.index-left-block ul {
+	  padding: 10px 15px;
+	}
+	.index-left-block li {
+	  padding: 5px;
+	}
+	.index-board-list {
+	  overflow: hidden;
+	}
+	.index-board-item {
+	  float: left;
+	  width: 43%;
+	  background: #fff;
+	  box-shadow: 0 0 1px #ddd;
+	  padding: 3%;
+	  margin-right: 2%;
+	  margin-bottom: 20px;
+	  border-radius: 0 0 10px 10px;
+	}
+	.index-board-item:nth-child(2n){margin-right: 0;}
+	.index-board-item dt{
+		float: left;
+		width: 125px;
+		height: 125px;
+		
+	}
+	.index-board-item dt img{width: 100%;height: 100%;}
+	.index-board-item:nth-child(1) dt{background: url(../assets/images/1.png) no-repeat center;}
+	.index-board-item:nth-child(2) dt{background: url(../assets/images/2.png) no-repeat center;}
+	.index-board-item:nth-child(3) dt{background: url(../assets/images/3.png) no-repeat center;}
+	.index-board-item:nth-child(4) dt{background: url(../assets/images/4.png) no-repeat center;}
+	
+	.index-board-item-inner {
+	  float: left;
+	  min-height: 125px;
+	  padding-left: 20px;
+	  line-height: 1.6;
+	}
+	
+	
+	.index-board-item h2 {
+	  font-size: 18px;
+	  font-weight: bold;
+	  color: #000;
+	  margin-bottom: 15px;
+	}
+	.index-board-button {
+	  margin-top: 20px;
+	}
+	.lastest-news {
+	  min-height: 512px;
+	}
+	.hot-tag {
+	  background: #c04fb1;
+	  color: #fff;
+	  font-size: 12px;
+	  padding: 0 5px;
+	}
+	.new-item {
+	  display: inline-block;
+	  width: 230px;
+	  overflow: hidden;
+	  text-overflow: ellipsis;
+	  white-space: nowrap;
+	}
+	.VueCarousel {
+	  margin-bottom: 10px;
+	}
+	.VueCarousel-slide {
+	  position: relative;
+	  color: #fff;
+	  font-family: Arial;
+	  font-size: 24px;
+	  text-align: center;
+	  min-height: 100px;
+	  width: 100%;
+	}
+	.VueCarousel-slide img {
+	  width: 100%;
+	}
+	.VueCarousel-navigation-prev {
+	  left: 30px !important;
+	}
+	.VueCarousel-navigation-next {
+	  right: 30px !important;
+	}
+	.VueCarousel-pagination {
+	  position: absolute;
+	  bottom: 10px;
+	  z-index: 9;
+	}
+</style>
